68f78b673723cdca5ad149268ca5dffb914a870b
Ominini <omininiab@gmail.com>
2021-02-02 21:16:56 -0500

    mvp

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let myLibrary = [{ title: "Art of war", author: "Sun Tzu", read: false}];
+let myLibrary = [{ title: "Art of war", author: "Sun Tzu", read: false }, { title: "Prisoners of Geography", author: "Tim Marshall", read: true }, { title: "Antisocial", author: "Samuel Ominini", read: false }];
 
 class Book {
     constructor(title, author, read) {
@@ -12,11 +12,24 @@ class Book {
     }
 }
 
-function addBookToLibrary() {
-    addBook = document.getElementById("addBook")
-    
-    myLibrary.push(book)
+addBook = document.getElementById("addBook")
+addBook.onsubmit = function () {
+    title = document.getElementById("title")
+    author = document.getElementById("author")
+    readStatus = document.getElementsByName("readStatus")
+    bookTitle = title.value
+    bookAuthor = author.value
+    for (let i = 0; i < readStatus.length; i++) {
+        if (readStatus[i].checked) {
+            bookReadStatus = Boolean(readStatus[i].value)
+        }
+    }
+
+    myLibrary.push(new Book(bookTitle, bookAuthor, bookReadStatus))
+    addBook.reset();
+
     displayBooks()
+    return false
 }
 
 function removeBookFromLibrary() {
@@ -26,20 +39,25 @@ function removeBookFromLibrary() {
 
 function displayBooks() {
     myBooks = document.getElementById("myBooks")
+    myBooks.innerHTML = ""
     header = document.createElement("h2")
     header.innerText = "My Books"
-    list = document.createElement("ol")
+    books = document.createElement("div")
+    books.classList.add("w3-container")
+
     if (myLibrary.length > 0) {
         myBooks.appendChild(header)
-        myBooks.appendChild(list)
+        myBooks.appendChild(books)
 
-        i = 0;
+
         myLibrary.forEach(function (book) {
+            card = document.createElement("div")
+            card.classList.add("w3-card")
+            books.appendChild(card)
             console.log(book)
-            myBook = document.createElement("li")
+            myBook = document.createElement("h3")
             myBook.innerText = `${book["title"]} by ${book["author"]}`
-            list.appendChild(myBook)
-            i++
+            card.appendChild(myBook)
         })
     } else {
         warning = document.createElement("p")
